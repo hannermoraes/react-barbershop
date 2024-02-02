0979005cb223b88155d9bd3d6498ac34a8af45b2
Hanner Moraes <hannermoraes@gmail.com>
2024-02-02 11:46:11 -0300

    feat> add next auth setup

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,16 +1,25 @@
+"use client"
+
 import { MenuIcon } from "lucide-react"
 import { Button } from "./ui/button"
 import { Card, CardContent } from "./ui/card"
 import Image from "next/image"
+import { signIn } from "next-auth/react"
 
 const Header = () => {
+  const handleLoginClick = async () => {
+    await signIn()
+  }
+
   return (
     <Card>
       <CardContent className="p-5 justify-between items-center flex flex-row">
         <Image src="/logo.png" alt="FSW Barber" height={22} width={120}></Image>
-        <Button className="p-2 h-8 w-8" variant="outline" size="icon">
+        {/* <Button className="p-2 h-8 w-8" variant="outline" size="icon">
           <MenuIcon size={16} />
-        </Button>
+        </Button> */}
+
+        <Button onClick={handleLoginClick}>Login</Button>
       </CardContent>
     </Card>
   )
--- app/api/auth/[...nextauth]/router.ts
+++ app/api/auth/[...nextauth]/router.ts
@@ -1,17 +1,17 @@
-import NextAuth from "next-auth/next";
-import { db } from "@/app/_lib/prisma"
-import GoogleProvider from "next-auth/providers/google"
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { Adapter } from "next-auth/adapters";
+import NextAuth from "next-auth/next";
+import GoogleProvider from "next-auth/providers/google"
+import { db } from "@/app/_lib/prisma"
 
-
-const handle = NextAuth({
+const handler = NextAuth({
   adapter: PrismaAdapter(db) as Adapter,
-  providers: [GoogleProvider({
+  providers: [
+    GoogleProvider({
     clientId: process.env.GOOGLE_CLIENT_ID as string,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
   }),
   ],
 })
 
-export { handle as GET, handle as POST }
+export { handler as GET, handler as POST}
