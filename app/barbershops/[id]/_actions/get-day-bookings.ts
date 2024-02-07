daeeafb402fb8ad897e4ef134065b15b78fbff33
Hanner Moraes <hannermoraes@gmail.com>
2024-02-07 16:12:55 -0300

    feat: ensure only available hours are show on service booking

---
+++ app/barbershops/[id]/_actions/get-day-bookings.ts
@@ -0,0 +1,19 @@
+"use server"
+
+import { db } from "@/app/_lib/prisma"
+import { endOfDay, startOfDay } from "date-fns"
+
+export const getDayBookings = async (date: Date, barbershopId: string) => {
+  const bookings = await db.booking.findMany({
+    where: {
+      barbershopId,
+      date: {
+        lte: endOfDay(date),
+        gte: startOfDay(date),
+      },
+    },
+  });
+  
+  return bookings
+
+};
